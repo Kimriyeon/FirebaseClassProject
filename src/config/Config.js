4e9c4ab038387cdc8d6814f3bcb1e72a7a24038f
Kimriyeon <yeon860424@gmail.com>
2023-03-14 21:23:50 +1100

    add project and firebase

---
+++ src/config/Config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAk3UrzJJzadbNiz5l17lk5aiqlaT5VX-o",
+  authDomain: "classproject-93d2c.firebaseapp.com",
+  projectId: "classproject-93d2c",
+  storageBucket: "classproject-93d2c.appspot.com",
+  messagingSenderId: "199040506005",
+  appId: "1:199040506005:web:46265181c926cf12aa239c"
+};
