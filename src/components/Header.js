b2aa9ffed6bd50f126d9899413a7933ef77e50d0
Kimriyeon <yeon860424@gmail.com>
2023-04-18 19:31:45 +1000

    add header component

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import Navbar from "react-bootstrap/Navbar"
+import Container from "react-bootstrap/Container"
+
+export function Header(props) {
+    return (
+        <Navbar bg="dark" variant="dark" expand="lg">
+            <Container>
+                <Navbar.Brand>
+                    Hello Website
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="main-nav" />
+                <Navbar.Collapse id="main-nav">
+
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
+}
