93c60d3e0790e2653bca7dfe7f10e15c76e44c0e
Kimriyeon <yeon860424@gmail.com>
2023-05-24 01:05:14 +1000

    load data from firestore and add storage

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { firebaseConfig } from "./config/Config";
 import { initializeApp } from "firebase/app";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 import { useState } from "react";
 
 import './App.css';
@@ -18,10 +19,13 @@ import { Routes, Route } from "react-router-dom";
 // contexts
 import { NavContext } from "./contexts/NavContext";
 import { FBAuthContext } from "./contexts/FBAuthContext";
+import { FBDbContext } from "./contexts/FBDbContext";
+import { FBStorageContext } from "./contexts/FBStorageContext";
 
 const FirebaseApp = initializeApp(firebaseConfig);
 const FirebaseAuth = getAuth(FirebaseApp);
 const FirebaseDB = getFirestore(FirebaseApp);
+const FirebaseStorage = getStorage(FirebaseApp);
 
 const NavRoutes = [
   { name: "Home", goto: "/" },
@@ -59,16 +63,20 @@ function App() {
         <Header />
       </NavContext.Provider>
       <FBAuthContext.Provider value={FirebaseAuth}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/signin" element={<Signin />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/signout" element={<SignOut />} />
-        </Routes>
+        <FBDbContext.Provider value={FirebaseDB}>
+          <FBStorageContext.Provider value={FirebaseStorage}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/signin" element={<Signin />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/signout" element={<SignOut />} />
+            </Routes>
+          </FBStorageContext.Provider>
+        </FBDbContext.Provider>
       </FBAuthContext.Provider>
-    </div>
+    </div >
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,60 @@
+import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
+
+import { useContext, useEffect, useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { ref } from "firebase/storage";
+
+import { FBDbContext } from "../contexts/FBDbContext";
+import { FBStorageContext } from "../contexts/FBStorageContext";
+
 export function Home() {
+    const [data, setData] = useState([])
+
+    const FBDb = useContext(FBDbContext)
+    const FBStorage = useContext(FBStorageContext)
+
+    const getData = async () => {
+        // get data from firestore collection called "books"
+        const querySnapshot = await getDocs(collection(FBDb, "books"))
+        // an array to store all the books from firestore
+        let books = []
+        querySnapshot.forEach((doc) => {
+            let book = doc.data()
+            book.id = doc.id
+            // add the book to the array
+            books.push(book)
+        })
+        // set the books array as the data state
+        setData(books)
+        console.log(books)
+    }
+
+    useEffect(() => {
+        if (data.length === 0) {
+            getData()
+        }
+    })
+
+    const Columns = data.map((book, key) => {
+        return (
+            <Col md="4" key={key}>
+                <Card>
+                    <Card.Body>
+                        <Card.Title>{book.title}</Card.Title>
+                    </Card.Body>
+                </Card>
+            </Col>
+        )
+    })
+
     return (
-        <div>
-            <h1>Home</h1>
-        </div>
+        <Container>
+            <Row>
+                {Columns}
+            </Row>
+        </Container>
     )
 }
